0d8df43d07c0e115b34a5a32f01ff04e74049f9d
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2015-06-21 15:04:34 +0300

    Add constructors and come skeletons

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,19 @@ struct Tower {
     weight : usize,
 }
 
+impl Tower {
+    pub fn new(weight: usize) -> Tower {
+        Tower { weight : weight }
+    }
+
+    pub fn weight(&self) -> usize {
+        self.weight
+    }
+}
+
 impl Display for Tower {
     fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
-        f.write_fmt(format_args!("[{0}]", iter::repeat("#").take(self.weight).collect::<String>()))
+        f.write_fmt(format_args!("[{0}]", iter::repeat("#").take(self.weight()).collect::<String>()))
     }
 }
 
@@ -16,15 +26,43 @@ struct Rod {
     stack : Vec<Tower>,
 }
 
+impl Rod {
+    pub fn new(sizes : &[usize]) -> Rod {
+        Rod { stack : sizes.iter().map(|x| Tower::new(*x) ).collect::<Vec<_>>() }
+    }
+
+    fn can_take_tower(&self, tower : &Tower) -> bool {
+        match self.stack.last() {
+            None => true,
+            Some(t) => t.weight() > tower.weight(),
+        }
+    }
+
+    pub fn can_take(&self, other: &Rod) -> bool {
+        false // self.can_take_tower(&other.stack.last())
+    }
+
+    pub fn swap(& mut self, other: &mut Rod) -> &mut Self {
+        self
+    }
+
+}
+
 struct Desk {
     rods : Vec<Rod>,
 }
 
+impl Desk {
+    pub fn new(rods: &[&[usize]]) -> Desk {
+        Desk { rods : rods.iter().map(|x| Rod::new(*x) ).collect::<Vec<_>>() }
+    }
+}
+
 impl Display for Desk {
     fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
-        let max_weight : usize = 
+        let max_weight : usize =
             self.rods.iter()
-                .map(|r| r.stack.iter().map(|s| s.weight).max().unwrap_or(0))
+                .map(|r| r.stack.iter().map(|s| s.weight()).max().unwrap_or(0))
                 .max().unwrap_or(0);
         let height = max_weight + 1;
 
@@ -38,7 +76,7 @@ impl Display for Desk {
                 let stower: Option<&Tower> = rod.stack.get(i);
                 let tmp = match stower {
                     None => format!("{0}|{0}", dup(" ", max_weight + 1)),
-                    Some(tower) => format!("{1}[{0}|{0}]{1}", dup("#", tower.weight), dup(" ", max_weight - tower.weight)),
+                    Some(tower) => format!("{1}[{0}|{0}]{1}", dup("#", tower.weight()), dup(" ", max_weight - tower.weight())),
                 };
                 s.push_str(" ");
                 s.push_str(&tmp);
@@ -55,29 +93,10 @@ impl Display for Desk {
 }
 
 fn main() {
-    let desk1 = Desk {
-        rods : vec![
-            Rod { stack:  vec![ Tower {weight: 4, }, Tower {weight: 3, }, ], },
-            Rod { stack:  vec![ Tower {weight: 2, }, ], },
-            Rod { stack:  vec![ Tower {weight: 1, }, ], },
-        ],
-    };
+    let desk1 = Desk::new(&[
+        &[4, 3],
+        &[2, 1]
+    ]);
     println!("{0}", desk1);
 
-    let desk2 = Desk {
-        rods : vec![
-            Rod { stack:  vec![ Tower {weight: 4, }, Tower {weight: 2, }, ], },
-            Rod { stack:  vec![ Tower {weight: 3, }, ], },
-            Rod { stack:  vec![ Tower {weight: 1, }, ], },
-        ],
-    };
-    println!("{0}", desk2);
-
-    let desk3 = Desk {
-        rods : vec![
-            Rod { stack:  vec![ Tower {weight: 4, }, Tower {weight: 2, }, ], },
-            Rod { stack:  vec![ Tower {weight: 3, }, Tower {weight: 1 }, ], },
-        ],
-    };
-    println!("{0}", desk3);
 }
