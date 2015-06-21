d3e0ad5a990399fa40a5075e1ec9cdaf0ad9b70f
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2015-06-21 12:02:28 +0300

    Incremental stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::fmt;
+use std::fmt::Display;
+use std::iter;
+
+struct Tower {
+    weight : usize,
+}
+
+impl Display for Tower {
+    fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
+        f.write_fmt(format_args!("[{0}]", iter::repeat("#").take(self.weight).collect::<String>()))
+    }
+}
+
+struct Rod {
+    stack : Vec<Tower>,
+}
+
+struct Desk {
+    rods : Vec<Rod>,
+}
+
+impl Display for Desk {
+    fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
+        let max_weight = 
+            self.rods.iter()
+                .map(|r| r.stack.iter().map(|s| s.weight).max().unwrap_or(0))
+                .max().unwrap_or(0);
+        let height = max_weight + 2usize;
+
+        let width = 2 + ((max_weight + 2) * self.rods.len()) + 2;
+        let mut display : Vec<String> = Vec::new();
+
+        display.reverse();
+        f.write_fmt(format_args!("{0}", display.connect("")))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let t  =  Tower { weight : 10,  };
+    println!("Hello, world! {0}", t);
 }
